32a3d1821caecd674e2d68a97fcc6b3f87f335ea
Mohamed <thisismohalawa@gmail.com>
2022-11-07 00:05:15 +0200

    Master empty project..

---
+++ app/src/main/java/sparespark/crypto/currency/MainApplication.kt
@@ -0,0 +1,7 @@
+package sparespark.crypto.currency
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainApplication : Application()
---
+++ app/src/main/java/sparespark/crypto/currency/presentation/MainActivity.kt
@@ -0,0 +1,17 @@
+package sparespark.crypto.currency.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+            /*
+            * Empty project ..
+            * */
+        }
+    }
+}
